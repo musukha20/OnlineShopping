dd6d11b97a0acd9e549c9f0d07acb29d36a339e6
AbheekRawal <abkrawal29@gmail.com>
2020-09-25 11:38:53 +0530

    Created a new package with a class Test and modified user class

--- ProjectGladiatorOnlineShopping/src/main/java/com/lti/entity/User.java
+++ ProjectGladiatorOnlineShopping/src/main/java/com/lti/entity/User.java
@@ -3,6 +3,11 @@ package com.lti.entity;
 public class User {
 	private int uid;
 	private String uname;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("hey Abheek here");
+	}
 
 
 }
---
+++ ProjectGladiatorOnlineShopping/src/main/java/com/lti/test/Test.java
@@ -0,0 +1,9 @@
+package com.lti.test;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		System.out.println("heyo abheek was here again");
+	}
+}
