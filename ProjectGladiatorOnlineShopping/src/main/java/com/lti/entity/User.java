1c7f85ef132871a3251dced4f8096328c9ce26ea
musukha20 <musukhab2@gmail.com>
2020-09-25 11:27:38 +0530

    new commit

--- ProjectGladiatorOnlineShopping/src/main/java/com/lti/entity/User.java
+++ ProjectGladiatorOnlineShopping/src/main/java/com/lti/entity/User.java
@@ -1,14 +1,8 @@
 package com.lti.entity;
 
 public class User {
-<<<<<<< HEAD
-
 	private int uid;
 	private String uname;
-=======
-	private int id;
-	private String name;
-	
 
->>>>>>> branch 'master' of https://github.com/musukha20/OnlineShopping.git
+
 }
