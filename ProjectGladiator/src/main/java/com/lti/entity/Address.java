eab08c0f03f568331565f02563b094a19ae91df4
Samyak <samyakdarshan97@gmail.com>
2020-09-26 00:49:39 +0530

    Address and Product Commit Done

---
+++ ProjectGladiator/src/main/java/com/lti/entity/Address.java
@@ -0,0 +1,48 @@
+package com.lti.entity;
+
+public class Address {
+
+	private int addressId;
+	private String state;
+	private String city;
+	private int pincode;
+	private String type;
+	private int userId;
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+}
--- ProjectGladiator/src/main/java/com/lti/entity/Product.java
+++ ProjectGladiator/src/main/java/com/lti/entity/Product.java
@@ -1,5 +1,54 @@
 package com.lti.entity;
 
 public class Product {
-
+	private int productId;
+	private String productName;
+	private double productPrice;
+	private int productQuantity;
+	private int categoryId;
+	private int retailerId;
+	private int cartId;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	public int getProductQuantity() {
+		return productQuantity;
+	}
+	public void setProductQuantity(int productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getRetailerId() {
+		return retailerId;
+	}
+	public void setRetailerId(int retailerId) {
+		this.retailerId = retailerId;
+	}
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	
 }
