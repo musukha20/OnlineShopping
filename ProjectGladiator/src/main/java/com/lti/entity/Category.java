29a5923cf47a90379c076a42322496bbb3430f06
AbheekRawal <abkrawal29@gmail.com>
2020-09-26 00:58:47 +0530

    User and Category Entity Committed.

---
+++ ProjectGladiator/src/main/java/com/lti/entity/Category.java
@@ -0,0 +1,21 @@
+package com.lti.entity;
+
+public class Category {
+	
+	private int categoryId;
+	private String name;
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
--- ProjectGladiator/src/main/java/com/lti/entity/User.java
+++ ProjectGladiator/src/main/java/com/lti/entity/User.java
@@ -1,6 +1,61 @@
 package com.lti.entity;
 
+import java.time.LocalDate;
+
 public class User {
+	
+	private int userId;
+	private LocalDate dateOfBirth;
+	private String email;
+	private int phoneNo;
+	private String password;
+	private int creditCard;
+	private int cartId;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(int creditCard) {
+		this.creditCard = creditCard;
+	}
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	
+	
+
 
 	
 }
